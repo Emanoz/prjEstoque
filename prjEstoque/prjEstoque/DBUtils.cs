1a5a34923944148b033c23fca8c02e7c2931bb6c
Emanuel Martins Lourencon <emanuel.lourencon@facens.br>
2019-08-09 22:20:06 -0300

    Criação da classe de conecção ao banco de dados

--- prjEstoque/prjEstoque/DBUtils.cs
+++ prjEstoque/prjEstoque/DBUtils.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +14,66 @@ namespace prjEstoque
         private SqlConnection conn = null;
         public DBUtils()
         {
-            conn = 
+            conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString);
+        }
+        public void OpenConnection()
+        {
+            if(conn.State == ConnectionState.Closed)
+                conn.Open();
+        }
+        public void CloseConnection()
+        {
+            if (conn.State == ConnectionState.Open)
+                conn.Close();
+        }
+        public void CallExecuteNonQuery(string query)
+        {
+            try
+            {
+                using(SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    OpenConnection();
+                    cmd.ExecuteNonQuery();
+                    CloseConnection();
+                }
+            }
+            catch(SqlException sqlex)
+            {
+
+                throw new Exception("Erro ao executar o comando!\n'" + sqlex + "'");
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+        public SqlDataReader CallExecuteReader(string query)
+        {
+            try
+            {
+                using(SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.CommandType = CommandType.Text;
+                    OpenConnection();
+                    return cmd.ExecuteReader(CommandBehavior.CloseConnection);
+                }
+            }
+            catch (SqlException sqlex)
+            {
+
+                throw new Exception("Erro ao executar o comando!\n'" + sqlex + "'");
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+        ~DBUtils()
+        {
+            conn.Dispose();
         }
     }
 }
