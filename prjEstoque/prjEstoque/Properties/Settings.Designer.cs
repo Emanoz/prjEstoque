56ae36281be5d66a96b2b21ecdda1d8b47b88782
EMANUEL MARTINS LOURENCON <180281@facens.br>
2019-10-02 10:02:18 -0300

    Connections string do BD da facens modificado

--- prjEstoque/prjEstoque/Properties/Settings.Designer.cs
+++ prjEstoque/prjEstoque/Properties/Settings.Designer.cs
@@ -26,7 +26,7 @@ namespace prjEstoque.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Server=PC-NANA\\SQLEXPRESS;Database=bdEstoque;Trusted_Connection=True;")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=sol;Initial Catalog=180281;Integrated Security=True")]
         public string connString {
             get {
                 return ((string)(this["connString"]));
